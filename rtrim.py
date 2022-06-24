991acf732610ffa4012ee160cbc3ce3ca6a6735e
Filip Wiśniewski <filip@badgermapping.com>
2022-06-24 14:59:48 +0200

    Trim signs from the end of text followed by newline

--- rtrim.py
+++ rtrim.py
@@ -1,5 +1,12 @@
 def right_trim(text: str):
-    # deletes tabs and spaces at the end of every line
-    if text[-1] in [' ', '\t']:
+    # deletes tabs and spaces at the end of each text line
+    chars_to_delete = [' ', '\t']
+
+    if text[-1] in chars_to_delete:
         return text[:-1]
+
+    elif text[-1] == '\n' and text[-2] in chars_to_delete:
+        endline_sign = '\n'
+        return text[:-2] + endline_sign
+
     return text
--- test_rtrim.py
+++ test_rtrim.py
@@ -7,35 +7,41 @@ class TestRtrim:
         assert True is True
 
     def test_returns_abc_when_abc_passed(self):
-        argument = "abc"
+        text = "abc"
 
-        actual = right_trim(argument)
+        actual = right_trim(text)
         expected = "abc"
 
         assert actual == expected
 
     def test_returns_bcd_when_bcd_passed(self):
-        argument = "bcd"
+        text = "bcd"
 
-        actual = right_trim(argument)
+        actual = right_trim(text)
         expected = "bcd"
 
         assert actual == expected
 
     def test_deletes_space_from_the_end_of_string(self):
-        argument = "bcd "
+        text = "bcd "
 
-        actual = right_trim(argument)
+        actual = right_trim(text)
         expected = "bcd"
 
         assert actual == expected
 
     def test_deletes_tab_from_the_end_of_string(self):
-        argument = "bcd\t"
+        text = "bcd\t"
 
-        actual = right_trim(argument)
+        actual = right_trim(text)
         expected = "bcd"
 
         assert actual == expected
 
-    def test_
+    def test_deletes_signs_from_the_end_of_line_followed_by_unix_newline_sign(self):
+        text = "bcd\t\n"
+
+        actual = right_trim(text)
+        expected = "bcd\n"
+
+        assert actual == expected
